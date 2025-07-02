7b34cfeead3033e703e865c71afc68ba33e69262
darkness427Mm <darknessmax100@gmail.com>
2025-07-03 00:24:02 +0330

    Update SettingsActivity.java

--- app/src/main/java/com/hololo/app/dnschanger/settings/SettingsActivity.java
+++ app/src/main/java/com/hololo/app/dnschanger/settings/SettingsActivity.java
@@ -1,61 +1,17 @@
 package com.hololo.app.dnschanger.settings;
 
 import android.os.Bundle;
-import android.preference.PreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.widget.FrameLayout;
-
-import com.hololo.app.dnschanger.R;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import dagger.hilt.android.AndroidEntryPoint
-    
+import com.hololo.app.dnschanger.R;
+import dagger.hilt.android.AndroidEntryPoint; // نقطه ویرگول به این خط اضافه شد
+
 @AndroidEntryPoint
 public class SettingsActivity extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.fragment_container)
-    FrameLayout fragmentContainer;
-
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_settings);
-        ButterKnife.bind(this);
-
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setTitle(R.string.settings);
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
-
-        getFragmentManager().beginTransaction()
-                .replace(R.id.fragment_container, new SettingsFragment())
-                .commit();
+        setContentView(R.layout.activity_settings); // این یک layout فرضی است
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                finish();
-                break;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-
-    public static class SettingsFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-
-            addPreferencesFromResource(R.xml.preference_layout);
-        }
-    }
-
 }
