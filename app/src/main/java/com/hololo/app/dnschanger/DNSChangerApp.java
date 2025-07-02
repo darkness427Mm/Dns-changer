02abcd5867e272a44028590c38e9886d3865ac15
darkness427Mm <darknessmax100@gmail.com>
2025-07-03 00:29:03 +0330

    Update DNSChangerApp.java

--- app/src/main/java/com/hololo/app/dnschanger/DNSChangerApp.java
+++ app/src/main/java/com/hololo/app/dnschanger/DNSChangerApp.java
@@ -1,11 +1,9 @@
 package com.hololo.app.dnschanger;
 
 import android.app.Application;
-
 import dagger.hilt.android.HiltAndroidApp;
 
 @HiltAndroidApp
 public class DNSChangerApp extends Application {
     // Hilt به صورت خودکار همه چیز را مدیریت می‌کند.
-    // دیگر نیازی به هیچ کدی در اینجا نیست.
 }
